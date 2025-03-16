ef9928e0babee3af897d608809ce2ef5db06f889
Ubuntu <ubuntu@ip-172-31-1-155.ec2.internal>
2025-03-16 12:59:36 +0000

    fixed docker file

--- app.js
+++ app.js
@@ -5,4 +5,4 @@ app.get('/', (req, res) => {
     res.send('Hello, Jenkins with Docker!');
 });
 
-app.listen(3000, () => console.log('App running on port 3000'));
+app.listen(3000, '0.0.0.0', () => console.log('App running on port 3000'));
