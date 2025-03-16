64305344fce651f16e51494fdf63be6d04c6926b
Refilwe mohlala <refilwemohlalaa@gmail.com>
2025-03-16 16:00:03 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello, Jenkins with Docker!');
+    res.send('Hello Again, Jenkins was triggered after the git push command!');
 });
 
 app.listen(3000, '0.0.0.0', () => console.log('App running on port 3000'));
